985a34d59c27575fc653175c651acf1b17acf8c9
Tom Koltunov <tomkoltunov@gmail.com>
2025-10-16 19:26:35 +0300

    Aleychem

--- src/App.tsx
+++ src/App.tsx
@@ -30,6 +30,8 @@ const App: React.FC = () => {
     return <p>❌ שגיאה בטעינת הנתונים</p>;
   }
 
+  console.log("Aleychem");
+
   // אם הכל תקין – מציגים את הנתוניםf
   return (
     <div style={{ padding: '20px' }}>
